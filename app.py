90b9c5ef4709dd9248de30b137c09d446237fd31
Ashok Timsina <ashoktimsina147181@gmail.com>
2024-02-25 14:57:53 +0100

    Database upload setup DONE

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from inference_sdk import InferenceHTTPClient
+from werkzeug.utils import secure_filename
 import json
 import os
 import sqlite3 
@@ -14,8 +15,7 @@ cursor = connect.cursor()
 cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='items'")
 if cursor.fetchone()[0] == 0:
     connect.execute(
-        "CREATE TABLE items ( itemId INTEGER PRIMARY KEY, itemName TEXT NOT NULL, itemDescription TEXT, itemPrice REAL NOT NULL, itemFaceType TEXT )"
-    ) 
+        "CREATE TABLE items ( itemId INTEGER PRIMARY KEY, itemName TEXT NOT NULL, itemDescription TEXT, itemPrice REAL NOT NULL, itemFaceType TEXT, imageName TEXT NOT NULL)") 
 connect.close()
 
 # Read the API key from the secrets.json file
@@ -57,26 +57,74 @@ def process_image():
     class_name = result['predictions'][0]['class']
 
     return render_template('results.html', result=class_name)
+    
 
-# Define the route for the add_item page
-@app.route('/add', methods=['GET', 'POST']) 
-def add_item(): 
-    if request.method == 'POST': 
-        item_name = request.form['itemName'] 
-        item_description = request.form['itemDescription'] 
-        item_price = request.form['itemPrice']
-        item_face_type = request.form['itemFaceType'] 
-  
-        with sqlite3.connect("database.db") as conn: 
-            cursor = conn.cursor() 
-            cursor.execute("INSERT INTO items \
-                            (itemName, itemDescription, itemPrice, itemFaceType) \
-                            VALUES (?, ?, ?, ?)",
-                           (item_name, item_description, item_price, item_face_type)) 
-            conn.commit() 
-        return redirect(url_for('main')) 
-    else: 
-        return render_template('add_item.html') 
+
+## setup for file UPLoads
+UPLOAD_FOLDER = 'assets/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+# Ensure the upload folder exists, create if not
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER, mode=777, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = os.path.abspath(os.path.dirname(UPLOAD_FOLDER))
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_item():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'itemImage' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['itemImage']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        # saving image to the upload folder
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        # Save other form data and file path to the database
+        if file and allowed_file(file.filename):
+            item_name = request.form['itemName']
+            item_description = request.form['itemDescription']
+            item_price = request.form['itemPrice']
+            item_face_type = request.form['itemFaceType']
+            filename = secure_filename(file.filename)
+            # Save other form data and file path to the database
+            with sqlite3.connect("database.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO items \
+                                (itemName, itemDescription, itemPrice, itemFaceType, imageName) \
+                                VALUES (?, ?, ?, ?, ?)",
+                               (item_name, item_description, item_price, item_face_type, filename))
+                conn.commit()
+            return redirect(url_for('main'))
+    else:
+        return render_template('add_item.html')
+
+# Function to fetch all items from the database
+def get_all_items():
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM items")
+        items = cursor.fetchall()
+    return items
+
+# Route to display all items
+@app.route('/items')
+def display_items():
+    items = get_all_items()
+    return render_template('display_items.html', items=items)
+
+
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8001, debug=True)
